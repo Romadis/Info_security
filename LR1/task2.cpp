5cb83417355cfcc4d0bbc77e82aba218268802d0
Romadis <romadis2024@mail.ru>
2024-09-15 15:18:59 +0300

    Create task2.cpp
    Added a code containing a ciphertext cyclically shifted 3 characters back

---
+++ LR1/task2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string s, s2, t; int i;
+    vector <string> str;
+    ifstream f("C://Users/vladc/OneDrive/Рабочий стол/input.txt"); 
+    ofstream h("C://Users/vladc/OneDrive/Рабочий стол/output.txt");
+
+    while (!f.eof())
+    {
+        getline(f, s);
+        stringstream s1(s);
+        while (s1 >> s2)
+        {
+            str.push_back(s2);
+        }
+        for (i = 0; i < str.size(); i++)
+        {
+            t = str[i];
+            rotate(t.rbegin(), t.rbegin() + 3, t.rend());
+            h << t;
+        }
+    }
+    f.close();
+    h.close();
+}
+
